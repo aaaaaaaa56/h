a495911f0f004995a53181f6677376c558aab280
aaaaaaaa56 <107304633+aaaaaaaa56@users.noreply.github.com>
2025-10-26 23:58:08 +0300

    Update careStore.js

--- careStore.js
+++ careStore.js
@@ -2,6 +2,6 @@ module.exports = {
     Token: process.env.TOKEN,
     Guild: process.env.GUILD_ID,
     Channel: process.env.CHANNEL_ID,
-    selfDeaf: process.env.SELF_DEAF === 'true', // true أو false
-    selfMute: process.env.SELF_MUTE === 'false'  // true أو false
+    selfDeaf: process.env.SELF_DEAF === 'true',
+    selfMute: process.env.SELF_MUTE === 'true'
 };
